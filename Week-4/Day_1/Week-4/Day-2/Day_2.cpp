621e3914098d82ea10660840c9c5db10b363c366
asif1951 <161422801+asif1951@users.noreply.github.com>
2024-11-18 23:33:53 +0600

    Add files via upload
    Added Week-4 Day-2 Codes

---
+++ Week-4/Day_1/Week-4/Day-2/Day_2.cpp
@@ -0,0 +1,108 @@
+//1
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n;
+    ll k;
+    cin>>n>>k;
+    vector<int>v(n);
+    for(int i=0; i<n; i++)
+    {
+        cin>>v[i];
+    }
+    int l=0,r=0,s=0;
+    ll sum=0;
+    while(r<n)
+    {
+        sum+=v[r];
+        if(sum<=k)
+        {
+            s=max(s,r-l+1);
+        }
+        else
+        {
+            sum-=v[l];
+            l++;
+        }
+        r++;
+    }
+    cout<<s<<'\n';
+}
+
+//F
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int m, n, o, x;
+        cin>>m>>n>>o;
+        int s=m-1;
+        if(n<o)
+            x=abs(n-o)+o-1;
+        else
+            x=n-1;
+        if(s==x)
+        {
+            cout<<3<<endl;
+        }
+        else if(s<x)
+        {
+            cout<<1<<endl;
+        }
+        else
+        {
+            cout<<2<<endl;
+        }
+    }
+}
+
+
+//G
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        string s;
+        cin>>s;
+        string s2;
+        for(int i=n-1; i>=0; i--)
+        {
+            if(s[i]=='0')
+            {
+                int m=s[i-1]-'0';
+                i--;
+                int m1=s[i-1]-'0';
+                i--;
+                int as=(m1*10)+m;
+                char ch=as+96;
+                s2.push_back(ch);
+            }
+            else
+            {
+                int n=s[i]-'0';
+                char ch=n+96;
+                s2.push_back(ch);
+            }
+        }
+        reverse(s2.begin(), s2.end());
+        cout<<s2<<endl;
+    }
+}
---
+++ Week-4/Day_1/Week-4/Day_1/Day_1.cpp
@@ -0,0 +1,158 @@
+//A
+/*#include<bits/stdc++.h>
+#define rcb cout<<"RCB"<<endl
+#define csk cout<<"CSK"<<endl
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int x,y;
+    cin>>x>>y;
+    if(x-y>=18)
+        rcb;
+    else
+        csk;
+}
+
+
+//B
+#include<bits/stdc++.h>
+//#define rcb cout<<"RCB"<<endl
+//#define csk cout<<"CSK"<<endl
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin>>t;
+    while(t--){
+    int x,y;
+    cin>>x>>y;
+    int div1,div2;
+    div1=x/3;
+    div2=y/3;
+    if(x%3==0||y%3==0){
+        cout<<0<<endl;
+    }
+    else{
+        if(x-(3*div1)==y-(3*div2)){
+            cout<<x-(3*div1)<<endl;
+        }
+        else{
+            cout<<abs((x-(3*div1))-(y-(3*div2)))<<endl;
+        }
+    }
+
+    }
+}
+
+
+//C
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;
+        int count = 0, count1 = 0;
+        char as = s[0];
+        if (as == '0')
+            count++;
+        else
+            count1++;
+        for (int i = 1; i < n; i++)
+        {
+            if (s[i] != as)
+            {
+                if (s[i] == '0')
+                    count++;
+                else
+                    count1++;
+                as = s[i];
+            }
+        }
+        cout << min(count, count1) << endl;
+    }
+}*/
+
+
+//Decode string
+/*#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        string s;
+        cin>>s;
+        string s2;
+        for(int i=n-1; i>=0; i--)
+        {
+            if(s[i]=='0')
+            {
+                int m=s[i-1]-'0';
+                i--;
+                int m1=s[i-1]-'0';
+                i--;
+                int as=(m1*10)+m;
+                char ch=as+96;
+                s2.push_back(ch);
+            }
+            else
+            {
+                int n=s[i]-'0';
+                char ch=n+96;
+                s2.push_back(ch);
+            }
+        }
+        reverse(s2.begin(), s2.end());
+        cout<<s2<<endl;
+    }
+}*/
+
+
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n;
+    ll k;
+    cin>>n>>k;
+    vector<int>v(n);
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+    }
+    int l=0,r=0,s=0;
+    ll sum=0;
+    while(r<n){
+        sum+=v[r];
+        if(sum<=k){
+            s=max(s,r-l+1);
+        }
+        else{
+            sum+=v[l];
+            l++;
+        }
+        r++;
+    }
+    cout<<s<<'\n';
+}
